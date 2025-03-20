cc27c66bb133aad13845f8ea518f721f1effa0cd
Ujvári Csongor <ujvari.csongor.21i@egressy.info>
2025-03-20 12:30:54 +0100

    Add project files.

---
+++ WpfApp8/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp8
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ServerConnection connection;
+        public MainWindow()
+        {
+            InitializeComponent();
+            start();
+        }
+        void start()
+        {
+            connection = new ServerConnection("http://127.1.1.1");
+        }
+        async void LoginClick(object s, EventArgs e)
+        {
+            bool temp = await connection.Login(usernameInput.Text, passwordInput.Password);
+            if (temp)
+            {
+                MessageBox.Show("Sikeres bejelentkezés");
+                afterlogin a = new afterlogin() { Top = this.Top, Left = this.Left, Visibility = Visibility.Visible };
+                this.Hide();
+                a.Show();
+                a.Closing += (ss, ee) =>
+                {
+                    this.Show();
+                };
+            }
+        }
+        async void RegClick(object s, EventArgs e)
+        {
+            bool temp = await connection.Register(usernameInput.Text, passwordInput.Password);
+            if (temp)
+            {
+                MessageBox.Show("Sikeres");
+            }
+        }
+
+    }
+}
---
+++ WpfApp8/ServerConnection.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Windows;
+
+namespace WpfApp8
+{
+    class ServerConnection
+    {
+        HttpClient client = new HttpClient();
+        string serverUrl = "";
+        public ServerConnection(string serverUrl)
+        {
+            this.serverUrl = serverUrl;
+        }
+        public async Task<bool> Login(string password, string username)
+        {
+            string url = serverUrl + "/login";
+            try
+            {
+                var jsonInfo = new
+                {
+                    loginUsername = username,
+                    loginPassword = password
+                };
+                string jsonStringified = JsonConvert.SerializeObject(jsonInfo);
+                HttpContent sendThis = new StringContent(jsonStringified, Encoding.UTF8, "Application/json");
+                HttpResponseMessage response = await client.PostAsync(url, sendThis);
+                response.EnsureSuccessStatusCode();
+                string result = await response.Content.ReadAsStringAsync();
+                JsonData data = JsonConvert.DeserializeObject<JsonData>(result);
+                Token.token = data.token;
+                return true;
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+
+            }
+            
+            return false;
+        }
+        public async Task<bool> Register(string password, string username)
+        {
+            string url = serverUrl + "/register";
+            try
+            {
+                var jsonInfo = new
+                {
+                    registerUsername = username,
+                    registerPassword = password
+                };
+                string jsonStringified = JsonConvert.SerializeObject(jsonInfo);
+                HttpContent sendThis = new StringContent(jsonStringified, Encoding.UTF8, "Application/json");
+                HttpResponseMessage response = await client.PostAsync(url, sendThis);
+                response.EnsureSuccessStatusCode();
+                string result = await response.Content.ReadAsStringAsync();
+                JsonData data = JsonConvert.DeserializeObject<JsonData>(result);
+                Token.token = data.token;
+                return true;
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+
+            }
+
+            return false;
+        }
+        public async Task<List<string>> Profiles()
+        {
+            List<string> all = new List<string>();
+            string url = serverUrl + "/profiles";
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                string result = await response.Content.ReadAsStringAsync();
+                all = JsonConvert.DeserializeObject<List<JsonData>>(result).Select(item => item.username).ToList();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+
+            }
+
+            return all;
+        }
+    }
+}
